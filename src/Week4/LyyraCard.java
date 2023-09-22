b0d8201b2c260e7ae90bf6141a1805eda661c242
Karina <karina.steinberg@voco.ee>
2023-09-22 15:00:07 +0300

    Week 4 start

---
+++ src/Week4/LyyraCard.java
@@ -0,0 +1,14 @@
+package Week4;
+
+public class LyyraCard {
+    private double balance;
+
+    public LyyraCard(double balanceAtStart) {
+        // write code here
+    }
+    public static void main(String[] args) {
+        LyyraCard card = new LyyraCard(50);
+        System.out.println(card);
+    }
+}
+
