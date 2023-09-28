9af5b21c0cc510ff282ab21c93ba4f8326fa7829
Karina <karina.steinberg@voco.ee>
2023-09-28 12:37:35 +0300

    Week 4

--- src/Week4/LyyraCard.java
+++ src/Week4/LyyraCard.java
@@ -4,11 +4,58 @@ public class LyyraCard {
     private double balance;
 
     public LyyraCard(double balanceAtStart) {
-        // write code here
+        this.balance = balanceAtStart;
     }
-    public static void main(String[] args) {
-        LyyraCard card = new LyyraCard(50);
-        System.out.println(card);
+
+    public void payEconomical() {
+        if(balance >= 2.50){
+            balance = balance - 2.50;
+        }
+    }
+
+    public void payGourmet() {
+        if(balance >= 4.00){
+            balance = balance - 4.00;
+        }
+    }
+    public double getBalance() {
+        return balance;
+    }
+
+    public void loadMoney(double amount) {
+       if(balance + amount <= 150 && amount > 0){
+            balance = balance + amount;
+        } else if(balance + amount > 150 && amount > 0) {
+            balance = 150;
+        }
+    }
+
+    public static class Main {
+        public static void main(String[] args) {
+            LyyraCard cardPekka = new LyyraCard(20);
+            LyyraCard cardBrian = new LyyraCard(30);
+
+            cardPekka.payGourmet();
+            System.out.println("Pekka:" + cardPekka.getBalance());
+
+            cardBrian.payEconomical();
+            System.out.println("Brian:" + cardBrian.getBalance());
+
+            cardPekka.loadMoney(20);
+            System.out.println("Pekka:" + cardPekka.getBalance());
+
+            cardBrian.payGourmet();
+            System.out.println("Brian:" + cardBrian.getBalance());
+
+            cardPekka.payEconomical();
+
+            cardPekka.payEconomical();
+            System.out.println("Pekka:" + cardPekka.getBalance());
+
+            cardBrian.loadMoney(50);
+            System.out.println("Brian:" + cardBrian.getBalance());
+
+        }
     }
 }
 
